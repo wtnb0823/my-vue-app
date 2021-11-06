bb1133879cef76f14d31176b4517ca6a66920ee7
darse73 <k.yu.713558.2016113@gmail.com>
2021-11-06 20:07:59 +0900

    Counter.vueを実装

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="hello-world">
+      <img alt="Vue logo" src="./assets/logo.png" />
+      <div class="hello-world__message">
+        {{ message }}
+      </div>
+    </div>
+    <TodoList></TodoList>
+    <Counter v-bind:initialValue="1000"></Counter>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue"
-
+import TodoList from "@/components/TodoList.vue"
+import Counter from "@/components/Counter.vue"
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    TodoList,
+    Counter,
   },
+  data: function () {
+    return {
+      message: "WebExpert Course Vue Template",
+    }
+  },
+  methods: {},
 }
 </script>
 
@@ -21,8 +34,16 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+}
+</style>
+
+<style scoped>
+.hello-world {
   margin-top: 60px;
+  text-align: center;
+}
+
+.hello-world .hello-world__message {
+  color: #2c3e50;
 }
 </style>
